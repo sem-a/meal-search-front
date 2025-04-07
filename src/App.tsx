2c4649f08370ac69cbcdb6a1071e7dc178f12c21
sem-a <aleksey.i99@mail.ru>
2025-04-07 12:52:04 +0300

    ci

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import React, { useState } from "react";
 import Layout from "./components/layout";
 import { useSearchRecipesMutation } from "./app/services/recipes";
 import { Recipes } from "./features/recipes/recipesSlice";
